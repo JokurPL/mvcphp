61dabd7b1fbfae335414f23ab620aa8076904a4b
JokurPL <simcitymrsuperslid@gmail.com>
2017-02-12 12:45:57 +0100

    3

--- src/app/routes.php
+++ src/app/routes.php
@@ -6,3 +6,6 @@ use Symfony\Component\Routing\Annotation\Route;
 $routes = new RouteCollection();
 
 $routes->add('home', new Route('/home/{page}', array('page' => 1)));
+$routes->add('list', new Route('/list'));
+
+return $routes;
--- web/index.php
+++ web/index.php
@@ -4,31 +4,38 @@
 
   use Symfony\Component\HttpFoundation\Request;
   use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\Routing;
 
   $request = Request::createFromGlobals();
+
+  $routes = include __DIR__ . '/../src/app/routes.php';
+
+  $context = new Routing\RequestContext();
+  $context->fromRequest($request);
+  $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
   // $controller = isset($_GET['controller']) ? $_GET['controller'] : 'home';
 
   // $controller = $request->get('controller', 'home');
 
-  $path = $request->getPathInfo();
+  // $path = $request->getPathInfo();
 
-  $response = new Response();
+  // $response = new Response();
 
-  $dir  = __DIR__ . '/../src/app/controllers/%s.php';
+  // $dir  = __DIR__ . '/../src/app/controllers/%s.php';
   
-  $map = array(
-    '/home' => 'home',
-    '/list' => 'list'
-  );
+  // $map = array(
+  //   '/home' => 'home',
+  //   '/list' => 'list'
+  // );
 
-  if (isset($map[$path])) {
-    ob_start();
-    require sprintf($dir, $map[$path]);
-    $response->setContent(ob_get_clean());
-  } else {
-    $response->setStatusCode(404);
-    $response->setContent('Nie ma takiej strony.');
-  }
+  // if (isset($map[$path])) {
+  //   ob_start();
+  //   require sprintf($dir, $map[$path]);
+  //   $response->setContent(ob_get_clean());
+  // } else {
+  //   $response->setStatusCode(404);
+  //   $response->setContent('Nie ma takiej strony.');
+  // }
 
-  $response->send();
+  // $response->send();
